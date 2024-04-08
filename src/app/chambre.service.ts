189b065c15bf40e9cdf5a826264a0592ed5d13f1
dorraayari <dorra.ayari@esprit.tn>
2024-04-08 17:35:33 +0100

    first commit

---
+++ src/app/chambre.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChambreService {
+
+  private baseUrl = 'http://172.20.10.2:8087/chambre';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/findAll`);
+  }
+
+  add(universite: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/add`, universite);
+  }
+
+  update(id: number, universite: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/update/${id}`, universite);
+  }
+
+  delete(id: any): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/delete/${id}`);
+  }
+
+  getById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/findById/${id}`);
+  }
+}
---
+++ src/app/create/create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ChambreService } from '../chambre.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent {
+
+  chambre: any = {}; // Define a generic object to store form data
+
+  constructor(private chambreService: ChambreService, private router: Router) { }
+
+  addChambre(): void {
+    // Implement your create logic here
+    console.log(this.chambre); // Log the data for testing
+    // Example: Call a service method to create data
+     this.chambreService.add(this.chambre)
+       .subscribe(() => {
+         this.router.navigate(['/read']); // Navigate back to the read page after creation
+       });
+  }
+}
