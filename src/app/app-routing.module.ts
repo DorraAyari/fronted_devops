bd7913ad82d01f328a7d35a68749341027774bb5
DorraAyari <dorra.ayari@esprit.tn>
2024-04-14 19:37:30 +0100

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
 import { ReadComponent } from './read/read.component';
+
 import { UpdateComponent } from './update/update.component';
 
 const routes: Routes = [
